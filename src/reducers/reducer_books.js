32ab718dd775812274729974c88533189f492846
sahil <sahils@liftoffllc.com>
2016-05-10 18:23:54 +0530

    Booklist example

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    {title: 'You dont know javascript', pages: '10'},
+    {title: 'Encyclopedia', pages: '102'},
+    {title: 'Jason Bourne', pages: '401'},
+    {title: 'Mahabharata', pages: '1570'},
+    {title: 'Sachin Biography', pages: '690'}
+  ]
+}
